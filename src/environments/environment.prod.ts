d8c025ec5cbc5fbabeab5e331e6c4b5bfc3a9780
Faber Ordoñez <faberordonez1@gmail.com>
2021-11-21 00:42:09 -0500

    commit inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAhKTQ9xccLRKLV15SZ2DEziNTDH75seFQ",
+    authDomain: "usuarios-e46c2.firebaseapp.com",
+    projectId: "usuarios-e46c2",
+    storageBucket: "usuarios-e46c2.appspot.com",
+    messagingSenderId: "350119008475",
+    appId: "1:350119008475:web:0b8b1625bbd999fb78d4fe"
+  }
 };
